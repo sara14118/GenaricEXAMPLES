4b5f65cb852e0907a122dd5a3308a222ef4d1615
sara <sara.alzadjali14@outlook.com>
2023-12-05 13:46:01 +0400

    Add project files.

---
+++ GenaricEXAMPLE/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenaricEXAMPLE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("sara");
+            Print(23);
+            Print(2.4);
+            Print(true);
+        }
+
+
+        static void Print<T>(T val)
+        {
+            Console.WriteLine(typeof(T));
+            Console.WriteLine(val);
+        }
+    }
+}
